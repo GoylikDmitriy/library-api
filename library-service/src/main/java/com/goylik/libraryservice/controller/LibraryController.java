9f3fe259556fb3db6d072bee9e8d063796b1cc4f
GoylikDmitriy <goylik.dmitriy@gmail.com>
2024-02-26 19:25:55 +0300

    logs

--- library-service/src/main/java/com/goylik/libraryservice/controller/LibraryController.java
+++ library-service/src/main/java/com/goylik/libraryservice/controller/LibraryController.java
@@ -4,6 +4,7 @@ import com.goylik.libraryservice.model.dto.LibraryBookDto;
 import com.goylik.libraryservice.model.dto.BookUpdateRequest;
 import com.goylik.libraryservice.service.LibraryService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,23 +13,29 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/library")
 @AllArgsConstructor
+@Slf4j
 public class LibraryController {
     private final LibraryService libraryService;
 
     @GetMapping("/available-books")
     public List<LibraryBookDto> getAvailableBooks() {
+        log.info("[GET]: Getting all available books.");
         return libraryService.getAvailableBooks();
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<String> updateBook(@PathVariable("id") long bookId,
                                              @RequestBody BookUpdateRequest bookDto) {
+        log.info("[PUT]: Updating book with id = {}", bookId);
+
         libraryService.updateBook(bookId, bookDto);
         return ResponseEntity.ok("Book was updated successfully.");
     }
 
     @PostMapping("/add")
     public ResponseEntity<String> addBook(@RequestBody LibraryBookDto bookDto) {
+        log.info("[POST]: Adding book to the library. Book = {}", bookDto);
+
         libraryService.addBook(bookDto);
         return ResponseEntity.ok("Book was added successfully.");
     }
--- library-service/src/main/java/com/goylik/libraryservice/service/impl/LibraryServiceImpl.java
+++ library-service/src/main/java/com/goylik/libraryservice/service/impl/LibraryServiceImpl.java
@@ -7,6 +7,7 @@ import com.goylik.libraryservice.repository.LibraryRepository;
 import com.goylik.libraryservice.service.LibraryService;
 import com.goylik.libraryservice.service.exception.BookNotFoundException;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +16,7 @@ import java.util.List;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class LibraryServiceImpl implements LibraryService {
     private final LibraryRepository libraryRepository;
     private final ModelMapper modelMapper;
@@ -22,6 +24,8 @@ public class LibraryServiceImpl implements LibraryService {
     @Override
     @Transactional(readOnly = true)
     public List<LibraryBookDto> getAvailableBooks() {
+        log.info("Getting all available books.");
+
         List<LibraryBook> availableBooks = libraryRepository.findAllAvailableBooks();
         return availableBooks.stream()
                 .map(b -> modelMapper.map(b, LibraryBookDto.class))
@@ -31,22 +35,31 @@ public class LibraryServiceImpl implements LibraryService {
     @Override
     @Transactional
     public void updateBook(long bookId, BookUpdateRequest bookDto) {
+        log.info("Updating book with id = {}", bookId);
+
         LibraryBook book = libraryRepository.findById(bookId)
-                .orElseThrow(() -> new BookNotFoundException(
-                        String.format("No such book with id = %d in library.", bookId))
-                );
+                .orElseThrow(() -> {
+                    log.error("Error while fetching book by id: Book with id = {} is not found.", bookId);
+                    return new BookNotFoundException(String.format("No such book with id = %d in library.", bookId));
+                });
 
         book.setBorrowedTime(bookDto.getBorrowedTime());
         book.setReturnTime(bookDto.getReturnTime());
 
         libraryRepository.save(book);
+
+        log.info("Book was updated successfully.");
     }
 
     @Override
     @Transactional
     public void addBook(LibraryBookDto bookDto) {
+        log.info("Adding book to the library. Book = {}", bookDto);
+
         LibraryBook libraryBook = new LibraryBook();
         libraryBook.setBookId(bookDto.getBookId());
         libraryRepository.save(libraryBook);
+
+        log.info("Book was added to the library successfully.");
     }
 }
